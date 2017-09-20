1d3abf679d7e6f9f4d74503628b2a059fa220677
sugaryo <ryo.sugawara.1224@gmail.com>
2017-09-20 11:08:19 +0900

    内部処理を整理。

--- sharp4j/src/sharp4j/util/io/CrLfReader.java
+++ sharp4j/src/sharp4j/util/io/CrLfReader.java
@@ -5,28 +5,59 @@ import java.io.BufferedReader;
 
 public class CrLfReader implements AutoCloseable
 {
-	// TODO：内部での状態管理がイケてないので、BuffとReadのステータス管理を内部クラス化したい。
-
-	// TODO：フラグ二種は一方通行なので、実質３状態の有限オートマトンだからenumにしたい。
-
+	private static final char CR = '\r';
+	private static final char LF = '\n';
+	private static final char EMPTY = '\0';
+	
+	private static final int BUFF_SIZE = 256;
+	
+	
 	private final QuietBufferedReader reader;
 	private final StringBuilder sb;
-
-	private char[] buff = new char[256];
-	private int index = 256;
-	private int size = 0;
-
+	
+	
+	private class Buffer
+	{
+		private final char[] temp = new char[BUFF_SIZE];
+		
+		private int size = 0;
+		private int index = BUFF_SIZE;
+		private boolean eof = false;
+		
+		
+		public boolean fill()
+		{
+			if ( !eof )
+			{
+				size = reader.read( temp );
+				index = 0;
+				eof = -1 == size;
+			}
+			
+			return !eof;
+		}
+		
+		public char seek()
+		{
+			return index < size ? temp[index++] : EMPTY;
+		}
+	}
+	
+	private final Buffer buffer;
+	
+	
 	public CrLfReader(BufferedReader reader)
 	{
 		this.reader = new QuietBufferedReader( reader );
 		this.sb = new StringBuilder();
+		this.buffer = new Buffer();
 	}
-
-
+	
+	
 	public String next()
 	{
 		this.sb.setLength( 0 );
-
+		
 		if ( this.read() )
 		{
 			return this.sb.toString();
@@ -36,31 +67,26 @@ public class CrLfReader implements AutoCloseable
 			return null;
 		}
 	}
-
-
-	private static final char CR = '\r';
-	private static final char LF = '\n';
-
+	
+	
 	boolean end = false;
-
+	
 	private boolean read()
 	{
 		if ( this.end ) return false;
-
+		
 		boolean cr = false;
-
+		
 		do
 		{
-			for ( ; this.index < this.size; this.index++ )
+			char c;
+			while ( EMPTY != (c = buffer.seek()) )
 			{
-				char c = this.buff[this.index];
-
 				if ( cr )
 				{
 					// 前回のCrと今回のLfでCrLfが完成。
 					if ( LF == c )
 					{
-						this.index++;
 						return true;
 					}
 					// 前回のCrを一手遅れて格納。
@@ -69,38 +95,25 @@ public class CrLfReader implements AutoCloseable
 						sb.append( CR );
 					}
 				}
-
+				
 				// CR フラグを更新。
 				cr = CR == c;
-
+				
 				if ( !cr )
 				{
 					sb.append( c );
 				}
 			}
 		}
-		while ( fill() );
-
+		while ( buffer.fill() );
+		
+		
 		// fill で false が帰ってきた周回で end フラグを立てる。
 		this.end = true;
 		return true;
 	}
-
-	private boolean eof = false;
-
-	private boolean fill()
-	{
-		if ( eof ) return false;
-
-		int n;
-		this.size = n = reader.read( this.buff );
-		this.index = 0;
-
-		eof = -1 == n;
-		return !eof;
-	}
-
-
+	
+	
 	@Override
 	public void close() throws Exception
 	{
