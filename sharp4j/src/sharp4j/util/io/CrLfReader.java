ab6aa713d05c73b7ca48125bf1dcd6a7a6bec701
sugaryo <ryo.sugawara.1224@gmail.com>
2017-09-21 11:40:16 +0900

    内部のバッファサイズを指定できるように改良。（デフォルト256、最小8）

--- sharp4j/src/sharp4j/util/io/CrLfReader.java
+++ sharp4j/src/sharp4j/util/io/CrLfReader.java
@@ -1,21 +1,30 @@
 package sharp4j.util.io;
 
+import static sharp4j.common.JavaUtil.*;
 
 import java.io.BufferedReader;
 
 public class CrLfReader implements AutoCloseable
 {
+	private static final int DEF_SIZE = 256;
+	private static final int MIN_SIZE = 16;
+	
 	private class Buffer
 	{
-		private static final int BUFF_SIZE = 256;
+		private final char[] temp;
 		
-		private final char[] temp = new char[BUFF_SIZE];
+		private int size ;
+		private int index;
+		private boolean eof;
 		
-		private int size = 0;
-		private int index = BUFF_SIZE;
-		private boolean eof = false;
-		
-		
+		public Buffer(int size)
+		{
+			this.temp = new char[max( size, MIN_SIZE )];
+			this.size = 0;
+			this.index = this.temp.length;
+			this.eof = false;
+		}
+
 		public boolean fill()
 		{
 			if ( !eof )
@@ -43,11 +52,14 @@ public class CrLfReader implements AutoCloseable
 	private final Buffer buffer;
 	private final StringBuilder sb;
 	
-	
 	public CrLfReader(BufferedReader reader)
 	{
+		this( reader, DEF_SIZE );
+	}
+	public CrLfReader(BufferedReader reader, int bufferSize)
+	{
 		this.reader = new QuietBufferedReader( reader );
-		this.buffer = new Buffer();
+		this.buffer = new Buffer( bufferSize );
 		this.sb = new StringBuilder();
 	}
 	
@@ -87,18 +99,19 @@ public class CrLfReader implements AutoCloseable
 	
 	private void readline(boolean cr)
 	{
-		// ■バッファ読み込み済みの文字を CrLf が完成する所まで読み進める。
-		while ( buffer.next() )
+		// ■バッファ読み込み済みの文字を CrLf が完成する所まで読み進める：
+		while ( this.buffer.next() )
 		{
-			final char c = buffer.seek();
+			final char c = this.buffer.seek();
 			
 			if ( cr )
 			{
 				// ★ CrLf が完成 ★
 				if ( LF == c ) return;
 				
+				// CrLfが完成しなかった場合は、単独のCr出現（←そんなデータ無いと思うけど）なので
 				// 前回のCrを一手遅れて格納。
-				sb.append( CR );
+				this.sb.append( CR );
 			}
 			
 			// CR フラグを更新。
@@ -106,18 +119,22 @@ public class CrLfReader implements AutoCloseable
 			
 			if ( !cr )
 			{
-				sb.append( c );
+				this.sb.append( c );
 			}
 		}
 		
-		// ■CrLf が完成せずバッファ読み込みループを抜けてきた場合
+		// ■CrLf が完成せずバッファ読み込みループを抜けてきた場合：
 		
-		if ( buffer.fill() )
+		// 内部バッファにファイルから追加読み込みを試行。
+		if ( this.buffer.fill() )
 		{
-			readline( cr ); // 再帰処理
+			// 追加読み込みできた場合は再帰処理してCrLfの完成（またはEOF）を目指す。
+			this.readline( cr );
 		}
+		// ファイル終端に達していた場合、ココまで処理したのがファイルの最終行。
 		else
 		{
+			// end フラグを立てて処理を終わる。
 			this.end = true;
 		}
 	}
